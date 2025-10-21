fc28402ff9de526685cd77bc28e4a669b686a187
Nofisat Ibrahim <inofisat5@gmail.com>
2025-10-21 09:01:02 +0100

    About and contact page added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const timeElement = document.getElementById("time")
 function updateTime() {
     const now = new Date();
    const hours = String(now.getHours()).padStart(2, "0");
@@ -6,7 +8,54 @@ function updateTime() {
     const milliseconds = String(now.getMilliseconds()).padStart(3, "0");
 
     const timeString = `${hours}:${minutes}:${seconds}.${milliseconds}`;
-    document.querySelector('[data-testid="test-user-time"]').textContent = timeString;
+    document.getElementById('time').textContent = timeString;
 }
+if (timeElement){
 setInterval(updateTime, 1000);
-updateTime();
+updateTime();}
+
+const form = document.querySelector("#contact-form");
+
+function validateField(field) {
+    const errorMessage = field.parentElement.querySelector(".error-message");
+    
+    if (!field.checkValidity()) {
+        const errorText = field.dataset.error || field.validationMessage;
+        errorMessage.textContent = errorText;
+        field.setAttribute('aria-invalid', 'true');
+        return false;
+    }
+    errorMessage.textContent = "";
+    field.setAttribute('aria-invalid', 'false');
+    return true;
+}
+
+form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const submitButton = form.querySelector('[data-testid="test-contact-submit"]');
+    submitButton.disabled = true;
+    submitButton.textContent = 'Submitting...';
+
+    const fields = form.querySelectorAll("input, textarea");
+    let isValid = true;
+    fields.forEach((field) => {
+        if (!validateField(field)) {
+            isValid = false;
+        }
+    });
+    if (isValid) {
+        const successMessage = document.querySelector("[data-testid='test-contact-success-message']");
+        if (successMessage) {
+            successMessage.textContent = "Form submitted successfully!";
+            form.reset();
+            setTimeout(() => {
+                successMessage.textContent = "";
+            }, 2000); 
+        }
+    }
+});
+    form.querySelectorAll("input, textarea").forEach(field => {
+        field.addEventListener("blur", () => validateField(field));
+    });
+
+});
