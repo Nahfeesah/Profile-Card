32defc8491eea22ffa76b248323c36d30cee2f8c
Nofisat Ibrahim <inofisat5@gmail.com>
2025-10-21 09:34:53 +0100

    contactpage Updated

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const timeElement = document.getElementById("time")
 function updateTime() {
     const now = new Date();
+    // const timeString =Date.now()
    const hours = String(now.getHours()).padStart(2, "0");
     const minutes = String(now.getMinutes()).padStart(2, "0");
     const seconds = String(now.getSeconds()).padStart(2, "0");
@@ -53,6 +54,9 @@ form.addEventListener("submit", async function (event) {
             }, 2000); 
         }
     }
+    
+    submitButton.disabled = false;
+    submitButton.textContent = 'Submit';
 });
     form.querySelectorAll("input, textarea").forEach(field => {
         field.addEventListener("blur", () => validateField(field));
